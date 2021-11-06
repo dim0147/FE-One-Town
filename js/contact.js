b2ebed82764d484a5c2b8326509c53bd39bc0165
dim0147 <provandam0369@gmail.com>
2021-11-06 11:08:29 +0800

    fix menu

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,14 @@
+const hambuger = document.getElementById('menu__hambuger');
+const menuList = document.getElementById('menu__list');
+
+let show = false;
+
+hambuger.addEventListener('click', () => {
+    if (!show) menuList.classList.add('menu__list--show');
+    else menuList.classList.remove('menu__list--show');
+
+    show = !show;
+});
 const ips = document.querySelectorAll('input[type="range"]');
 
 ips.forEach((e) => e.addEventListener('change', onChangeSlider));
--- js/works.js
+++ js/works.js
@@ -1,3 +1,14 @@
+const hambuger = document.getElementById('menu__hambuger');
+const menuList = document.getElementById('menu__list');
+
+let show = false;
+
+hambuger.addEventListener('click', () => {
+    if (!show) menuList.classList.add('menu__list--show');
+    else menuList.classList.remove('menu__list--show');
+
+    show = !show;
+});
 const btn = document.querySelector('.popup__btn');
 btn.addEventListener('click', () => document.querySelector('.popup').classList.add('animate__backOutDown'));
 
